d254d7f3c9452ecddbf07e9e2bbac0c7602a3e1d
Lyk0s032 <98297324+Lyk0s032@users.noreply.github.com>
2025-09-23 12:40:50 -0500

    cguno

--- src/components/union/compras/compras/requisicion.jsx
+++ src/components/union/compras/compras/requisicion.jsx
@@ -175,7 +175,7 @@ export default function ShowRequisicion(){
                                                             <td>
                                                                 <div className='about'>
                                                                     <span>Código Kit: {r.id}</span><br />
-                                                                    <span>CGUNO Kit: {r.item}</span><br />
+                                                                    <span>CGUNO Kit: {r.cguno}</span><br />
                                                                     <strong>{r.nombre}</strong>
                                                                 </div>
                                                             </td>
